90134a972f3037ae06fd7007d9f7cc8baddcdf31
Sultan Karybaev <sultankarybaev@macbook-air-sultan.local>
2018-11-21 23:36:51 +0600

    Initial Commit

---
+++ Kinoogon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kinoogon
+//
+//  Created by Sultan Karybaev on 11/21/18.
+//  Copyright © 2018 Sultan Karybaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
